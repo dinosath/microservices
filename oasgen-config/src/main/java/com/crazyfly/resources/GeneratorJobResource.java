1d7c3d3bc99bd84eeee961ecaac13c73e06d0093
Konstaninos Athanasiou <dinosath0@gmail.com>
2023-05-04 23:56:51 +0300

    add svelteui for frontend in oasgen-config microservice

---
+++ oasgen-config/src/main/java/com/crazyfly/resources/GeneratorJobResource.java
@@ -0,0 +1,13 @@
+package com.crazyfly.resources;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("/oas_gen/generate")
+public class GeneratorJobResource {
+
+    @POST
+    public void generate(){
+
+    }
+}
---
+++ oasgen-config/src/main/java/com/crazyfly/services/JobGeneratorService.java
@@ -0,0 +1,4 @@
+package com.crazyfly.services;
+
+public class JobGeneratorService {
+}
